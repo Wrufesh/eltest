621d455e17a6fbf0bb7bfd7ab192d72bfadb1f9d
wrufesh <wrufesh@gmail.com>
2015-03-21 13:32:11 +0545

    done with minor bugs

--- eltest.py
+++ eltest.py
@@ -1,5 +1,6 @@
 from lxml import html
 import requests
+from pymongo import MongoClient
 import re
 
 
@@ -10,38 +11,37 @@ def get_pages():
     x = re.split(' ', p[0])
     y = re.split(',', x[5])
     items = int(''.join(y))
-    i_per_page = int(x[3]) - int (x[1]) + 1
+    i_per_page = int(x[3]) - int(x[1]) + 1
     return items/i_per_page
 
 
 def write_row(lyst):
-    for i, data in enumerate(lyst):
-        f.write(data)
-        if i+1 != len(lyst):
-            f.write(',')
-    f.write('\n')
+    xtable = {'advicers': lyst[0],
+              'firm_joining': lyst[1],
+              'firm_leaving': lyst[2],
+              'aum': lyst[3],
+              'date': lyst[4]
+              }
+    x_table = db.x_table
+    xtable_id = x_table.inser_one(xtable).inserted_id
+    print xtable_id
 
 
 # Connect database here
-f = open('output.csv', 'w')
+client = MongoClient('mongodb://localhost:27017/')
+db = client.eltest_db
 
 for page in range(1, get_pages()):
     page = requests.get('http://data.investmentnews.com/aotm/' + '?PAGE=' + str(page))
     tree = html.fromstring(page.text)
     xquery = tree.xpath('//table[@class="dataTable display treeTable rwd-table"]')
 
-    # Writing table headers
-    # table_headers = xquery[0].xpath('thead//a/text()')
-    # write_row(table_headers)
-
     table_rows = xquery[0].xpath('tbody/tr')
     for row in table_rows:
         row_data = row.xpath('td/text()')
-        row_data.insert(0, '"' + row[0][0].text + '"')
-        
+        advicers = re.split(',', row[0][0])
+        row_data.insert(0, advicers)
+
         # write to database here
         write_row(row_data)
         print row_data
-
-
-f.close()
